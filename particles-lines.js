9d8eff4ad2c2fa80a8039694f41d333d39ff7ccd
Luca <mail@lucamegens.nl>
2021-01-05 15:49:39 +0100

    Added text link and added text hover animation

--- particles-lines.js
+++ particles-lines.js
@@ -3,10 +3,10 @@
 particlesJS("particles-js", {
   "particles": {
     "number": {
-      "value": 280,
+      "value": 50,
       "density": {
         "enable": true,
-        "value_area": 500
+        "value_area":100
       }
     },
     "color": {
@@ -15,48 +15,47 @@ particlesJS("particles-js", {
     "shape": {
       "type": "circle",
       "stroke": {
-        "width": 0,
+        "width": 2,
         "color": "#000000"
       },
       "polygon": {
         "nb_sides": 8
       },
       "image": {
-        "src": "img/github.svg",
-        "width": 100,
-        "height": 100
+        "width": 1920,
+        "height": 1080
       }
     },
     "opacity": {
-      "value": 0.5,
+      "value": 1,
       "random": false,
       "anim": {
-        "enable": false,
-        "speed": 1,
+        "enable": true,
+        "speed": 2,
         "opacity_min": 0.1,
         "sync": false
       }
     },
     "size": {
-      "value": 1,
+      "value": 1.5,
       "random": true,
       "anim": {
         "enable": false,
         "speed": 80,
-        "size_min": 0.1,
-        "sync": false
+        "size_min": 0.5,
+        "sync": true
       }
     },
     "line_linked": {
       "enable": true,
       "distance": 50,
       "color": "#ffffff",
-      "opacity": 0.4,
-      "width": 0.5
+      "opacity": 1,
+      "width": 1
     },
     "move": {
       "enable": true,
-      "speed": 0.8,
+      "speed": 1,
       "direction": "none",
       "random": false,
       "straight": false,
@@ -110,24 +109,3 @@ particlesJS("particles-js", {
   },
   "retina_detect": true
 });
-
-
-/* ---- stats.js config ---- */
-
-var count_particles, stats, update;
-stats = new Stats;
-stats.setMode(0);
-stats.domElement.style.position = 'absolute';
-stats.domElement.style.left = '0px';
-stats.domElement.style.top = '0px';
-document.body.appendChild(stats.domElement);
-count_particles = document.querySelector('.js-count-particles');
-update = function() {
-  stats.begin();
-  stats.end();
-  if (window.pJSDom[0].pJS.particles && window.pJSDom[0].pJS.particles.array) {
-    count_particles.innerText = window.pJSDom[0].pJS.particles.array.length;
-  }
-  requestAnimationFrame(update);
-};
-requestAnimationFrame(update);
